44f827fd776b5e35fd44ebeb1064a20c5fddf4f1
tsumaethan <tsumaethan20@gmail.com>
2024-03-24 15:33:40 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+const currentUser = 'Ethan';
+const welcomeMessage() {}
+
+// Define welcomeMessage function
+function welcomeMessage() {
+  return `Welcome to Flatbook, ${Ethan}!`;
+}
+
+// Define excitedWelcomeMessage function
+function excitedWelcomeMessage() {
+  return `WELCOME TO FLATBOOK, ${Ethan.toUpperCase()}!`;
+}
+
+// Define shortGreeting function
+function shortGreeting() {
+  return `Welcome, ${Ethan.charAt(0)}!`;
+}
+
+
